63a4efdab8453bfb1cb87a86b9ee58b7030bfb38
Richard Branson <rmbranson@gmail.com>
2014-10-30 18:52:43 +0000

    99 Bottle v1

---
+++ 03-Collections-and-loops/richardbranson/99_bottles.rb
@@ -0,0 +1,7 @@
+count = 99
+loop do
+             break if count < 1
+             puts "#{count} bottles of beer on the wall, #{count} bottles of beer."
+             count -= 1
+             puts "Take one down and pass it around, #{count} bottles of beer on the wall."
+end
